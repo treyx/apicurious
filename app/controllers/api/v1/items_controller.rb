2a3e1fe027bbb254336945dd9817ba6188ed380b
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-04 14:30:22 -0600

    created strong params and create action in controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,4 +8,15 @@ class Api::V1::ItemsController < ApplicationController
   def show
     respond_with Item.find(params[:id])
   end
+
+  def create
+    respond_with :api, :v1, Item.create(item_params)
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description)
+  end
 end
