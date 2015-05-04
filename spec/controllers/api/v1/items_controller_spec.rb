c1e6520ea6885b4cdebbb5719fff529c7fc2fd9d
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-05-04 10:39:36 -0600

    added spec for show method

--- spec/controllers/api/v1/items_controller_spec.rb
+++ spec/controllers/api/v1/items_controller_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Api::V1::ItemsController, type: :controller do
     expect(item[:description]).to eq("this is a cat.")
   end
 
+  it "responds to #show" do
+    item = Item.create(name: "cat", description: "this is a cat.")
+    get :show, format: :json, id: item.id
+
+    first_item = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(200)
+
+    expect(first_item[:name]).to eq("cat")
+    expect(first_item[:description]).to eq("this is a cat.")
+  end
+
 end
